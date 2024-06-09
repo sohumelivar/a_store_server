4ca9c4e4987410b08cc2b36e57f5c3a0cb2c1096
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2024-06-09 18:51:15 +0300

    fix User Slice

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -58,7 +58,6 @@ const refresh = async (req, res, next) => {
     try {
         const {refreshToken} = req.cookies;
         const userData = await userService.refresh(refreshToken);
-        if (userData.error) return res.redirect(process.env.CLIENT_URL);
         res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
         return res.json(userData);
     } catch (error) {
