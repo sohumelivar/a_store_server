ab097db4f4cd93333e4b88ab2e4cbffaaaf829ad
Givi Koyava <givikoyava@iMac-Givi.local>
2024-06-24 20:14:09 +0300

    added checking on some routes

--- middlewares/auth-middleware.js
+++ middlewares/auth-middleware.js
@@ -1,4 +1,5 @@
 const tokenService = require('../service/token-service');
+const ApiError = require("../exceptions/api-error")
 
 module.exports = function (req, res, next) {
     try {
@@ -6,21 +7,25 @@ module.exports = function (req, res, next) {
         console.log('authorizationHeader --- >>> ', authorizationHeader);
         if (!authorizationHeader) {
             console.log('tut 0 -------- >>>');
-            return res.sendStatus(401);
+            // return res.sendStatus(401);
+            throw ApiError.BadRequest('No access')
         }
         const accessToken = authorizationHeader.split(' ')[1];
         if (!accessToken) {
             console.log('tut 1 -------- >>>');
-            return res.sendStatus(401);
+            // return res.sendStatus(401);
+            throw ApiError.BadRequest('No access')
         }
         const userData = tokenService.validateAccessToken(accessToken);
         if (!userData) {
             console.log('tut 2 -------- >>>');
-            return res.sendStatus(401);
+            // return res.sendStatus(401);
+            throw ApiError.BadRequest('No access')
         }
         req.user = userData;
         next();
     } catch (error) {
-        console.log('⚛ --- ⚛ --- ⚛ --- ⚛ ---  >>> ☢ error:', error)
+        // console.log('⚛ --- ⚛ --- ⚛ --- ⚛ ---  >>> ☢ error:', error)
+        next(error);
     }
 }
